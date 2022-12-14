4bf003a05a141fa90e688b554679a428534a1b01
+ <stingeyandrewstown@live.com>
2022-12-14 14:13:27 -0600

    commit tree 2ced01f6524ed4091bdc3eb4c83310e2b373b866

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Andrew Cavanaugh is the champion";
+document.body.append(newHeader);
